bd8e8eeb1c0a6d45ff4f71967d0e3ae12c3a6438
Jeroen van Ottelen <jeroen_van_ottelen@hotmail.com>
2017-05-16 15:53:05 +0200

    remove sensor package

--- src/sensor/OccupationSensor.py
+++
@@ -1,6 +0,0 @@
-from sensor.Sensor import *
-
-class OccupationSensor(Sensor):
-
-    def __init__(self, name, type, gpioTrigger):
-        super(OccupationSensor, self).__init__(name, type, gpioTrigger)
