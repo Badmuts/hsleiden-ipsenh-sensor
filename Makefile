ab3b246744b04f17e5a475226bca5aac83d34a1d
Jeroen van Ottelen <jeroen_van_ottelen@hotmail.com>
2017-05-21 16:11:56 +0200

    ci files

--- Makefile
+++ Makefile
@@ -4,3 +4,10 @@ raspberry-jeroen:
 	lftp sftp://pi:raspberry@192.168.1.58  -e "put ../hsleiden-ipsenh-sensor.zip; bye"
 	rm -rf ../hsleiden-ipsenh-sensor.zip
 	ssh pi@192.168.1.58 "unzip hsleiden-ipsenh-sensor.zip -d ~/; rm -rf ~/hsleiden-ipsenh-sensor.zip; source hsleiden-ipsenh-sensor/venv/bin/activate; cd hsleiden-ipsenh-sensor/src/; python main.py; deactivate; exit"
+
+
+
+ci: ci-test
+
+ci-test:
+	ls
---
+++ src/test/data_test/DatabaseConnectionTest.py
@@ -0,0 +1,16 @@
+import unittest
+from data.DatabaseConnection import *
+
+class DatabaseConnectionTest(unittest.TestCase):
+
+    def setUp(self):
+        self.databaseConnection = DatabaseConnection()
+        pass
+
+    def test_hasApiKey(self):
+        apiKey = self.databaseConnection.hasApiKeys()
+        self.assertEqual(apiKey, False)
+
+
+if __name__ == '__main__':
+    DatabaseConnectionTest.main()
