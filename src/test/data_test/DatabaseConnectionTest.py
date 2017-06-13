e75cb63142b9514eb69324f03c559f1dd2acf169
Jeroen van Ottelen <jeroen_van_ottelen@hotmail.com>
2017-06-13 21:48:35 +0200

    add new functions to test

--- src/test/data_test/DatabaseConnectionTest.py
+++ src/test/data_test/DatabaseConnectionTest.py
@@ -1,19 +1,56 @@
 import unittest
 from data.DatabaseConnection import *
+import os
 
 class DatabaseConnectionTest(unittest.TestCase):
 
     def runTest(self):
-        print 'hoi'
+        pass
 
     def setUp(self):
+        self.dbPath = 'HUB_database.db'
         self.databaseConnection = DatabaseConnection()
-        pass
 
     def test_hasApiKey(self):
         apiKey = self.databaseConnection.hasApiKeys()
-        self.assertEqual(apiKey, False)
+        self.assertFalse(apiKey)
 
+    def test_registerHUB(self):
+        self.databaseConnection.registerHUB("API_KEY")
+        apiKey = self.databaseConnection.hasApiKeys()
+        self.assertTrue(apiKey)
+
+    def test_registerSensor(self):
+        sensors = []
+
+        sensors.append({
+            "id": 1,
+            "name": "in",
+            "sensorType": "hc-sr04",
+            "status": True,
+            "gpio_trigger": 17,
+            "gpio_echo": 18
+        })
+
+        sensors.append({
+            "id": 2,
+            "name": "out",
+            "sensorType": "hc-sr04",
+            "status": True,
+            "gpio_trigger": 22,
+            "gpio_echo": 23
+        })
+
+        self.databaseConnection.registerSensor(sensors)
+        result = self.databaseConnection.getSensors()
+        self.assertEqual(len(result), 2)
+
+
+    def tearDown(self):
+        dbExist = os.path.isfile(self.dbPath)
+        if (dbExist):
+            os.remove(self.dbPath)
+            pass
 
 if __name__ == '__main__':
-    DatabaseConnectionTest()
+    unittest.main()
